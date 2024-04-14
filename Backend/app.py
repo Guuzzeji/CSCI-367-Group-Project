0e5f5cb90401036b53ff26bbdd99b0e004d6e830
Guuzzeji <5lh8dl440f@gmail.com>
2024-04-13 20:41:32 -0700

    Adding some error handles to be in json

--- Backend/app.py
+++ Backend/app.py
@@ -10,6 +10,20 @@ app = Flask(__name__)
 app.register_blueprint(test)
 app.register_blueprint(api)
 
+@app.errorhandler(404)
+def not_found(e):
+      return {
+            "status": 404,
+            "error": "page does not exist"
+      }
+
+@app.errorhandler(503)
+def server_error(e):
+      return {
+            "status": 503,
+            "error": "server issues :("
+      }
+
 if __name__ == '__main__':
     print("Running Server --> "
           + "localhost:"
