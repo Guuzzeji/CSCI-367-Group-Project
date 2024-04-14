6dfcf12b71a8e3e31213cdafd6d109ec21dfd151
Guuzzeji <5lh8dl440f@gmail.com>
2024-04-14 14:02:41 -0700

    Cleaning note comment to be more clear on how url will work for search

--- Backend/router/api.py
+++ Backend/router/api.py
@@ -13,7 +13,7 @@ def manga_info(book_id: int):
 
 # ! Query setup
 # NOTE: limit number of manga return to be 25 to 50
-# * Example url /API/search?query=helloworld&type=author&page=10
+# * Example url /API/search?query=helloworld&type=author&lastbookid=10
 # - type = title | author | theme | genre, defualt is title
 # - lastbookid = some int, default is 1
 # - query = user search term, this is required to run
