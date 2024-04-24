ea5020afbfc53ee2feae5b4c3554759ee7bf1a6d
Guuzzeji <5lh8dl440f@gmail.com>
2024-04-23 18:47:19 -0700

    Creating basic page layout

--- Frontend/anime-search-app/src/pages/NotFoundError/NotFoundError.jsx
+++ Frontend/anime-search-app/src/pages/NotFoundError/NotFoundError.jsx
@@ -1,12 +1,25 @@
 import * as React from 'react';
 
-import { Text, ScaleFade } from '@chakra-ui/react';
+import { Text, Image } from '@chakra-ui/react';
+
+import './NotFoundError.css';
 
 function NotFoundError() {
 
     return (
-        <div>
-            404 not found
+        <div className="Background">
+            <div className="BackgroundOverlay">
+                <div className="CenterScreen">
+                    <Image
+                        className="GifImage"
+                        borderRadius="10"
+                        objectFit='cover'
+                        src='https://media.tenor.com/eH93eoeVWokAAAAC/twomad-fall.gif'
+                        alt='Dan Abramov'
+                    />
+                    <Text className="BigText">Sorry This Page Does Not Exist 😔</Text>
+                </div>
+            </div>
         </div>
     );
 }
