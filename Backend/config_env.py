e00af07d84ecc5cc13fd3c1eaea3288387617869
Guuzzeji <5lh8dl440f@gmail.com>
2024-04-26 12:55:01 -0700

    Copying over changes from frontendConnections to Backend

--- Backend/config_env.py
+++ Backend/config_env.py
@@ -1,25 +1,38 @@
 import os
 import sys
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 # ! == About ==
 # In charge of loading .env file and converting it into a dictionary that can be used to look up variables
 
-is_load_env = load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
-
-if not is_load_env:
-    print("[ERROR] .env file does not exist. Please create .env file at root of backend in order to use MYSQL. .env cannot be empty either")
-    sys.exit(1)
-
-FLASK_DEBUG = False
-if os.getenv("FLASK_DEBUG").lower() == "true":
-    FLASK_DEBUG = True
-
+# ! For easy of use, just hard coding everything
 config_env = {
-    "MYSQL_USERNAME": os.getenv('MYSQL_USERNAME'),
-    "MYSQL_PASSWORD":  os.getenv('MYSQL_PASSWORD'),
-    "MYSQL_HOST": os.getenv('MYSQL_HOST'),
-    "MYSQL_DATABASE": os.getenv('MYSQL_DATABASE'),
-    "MYSQL_PORT": os.getenv('MYSQL_PORT'),
-    "FLASK_DEBUG": FLASK_DEBUG
+    "MYSQL_USERNAME": "root",
+    "MYSQL_PASSWORD": "rootpassword",
+    "MYSQL_HOST": "localhost",
+    "MYSQL_DATABASE": "manga_db",
+    "MYSQL_PORT": 4040,
+    "FLASK_DEBUG": False
 }
+
+
+# is_load_env = load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
+
+# if not is_load_env:
+#     print("[ERROR] .env file does not exist. Please create .env file at root of backend in order to use MYSQL. .env cannot be empty either")
+#     sys.exit(1)
+
+# FLASK_DEBUG = False
+# if os.getenv("FLASK_DEBUG").lower() == "true":
+#     FLASK_DEBUG = True
+
+# config_env = {
+#     "MYSQL_USERNAME": os.getenv('MYSQL_USERNAME'),
+#     "MYSQL_PASSWORD":  os.getenv('MYSQL_PASSWORD'),
+#     "MYSQL_HOST": os.getenv('MYSQL_HOST'),
+#     "MYSQL_DATABASE": os.getenv('MYSQL_DATABASE'),
+#     "MYSQL_PORT": os.getenv('MYSQL_PORT'),
+#     "FLASK_DEBUG": FLASK_DEBUG
+# }
+
+
--- Backend/db/get_manga_details.py
+++ Backend/db/get_manga_details.py
@@ -1,4 +1,6 @@
-from db.connection import CONNECTION_TO_DB
+from connection import CONNECTION_TO_DB
+# import datetime
+# from searchSQL.pageThree import getMangaDetails
 
 # ! == About ==
 # Handle getting more information about specifc manga in db
@@ -8,9 +10,54 @@ from db.connection import CONNECTION_TO_DB
 def get_manga_details(book_id: int):
     cursor = CONNECTION_TO_DB.cursor()
 
+    page_three_query = '''
+            SELECT DISTINCT m.title, a.Name, DATE_FORMAT(m.publishedstart, '%m/%d/%Y') AS published_start,
+            DATE_FORMAT(m.publishedend, '%m/%d/%Y') AS published_end,
+            GROUP_CONCAT(DISTINCT t.type SEPARATOR ', ') AS themes,
+            GROUP_CONCAT(DISTINCT g.type SEPARATOR ', ') AS genres,
+            m.synopsis
+            FROM Manga m
+            INNER JOIN Author a ON a.bookid = m.bookid
+            INNER JOIN Theme t ON t.bookid = a.bookid
+            INNER JOIN Genre g ON g.bookid = t.bookid
+            WHERE m.bookid = %s
+            GROUP BY m.title, a.Name, m.publishedstart, m.publishedend, m.synopsis
+        '''
+
+    cursor.execute(page_three_query, (book_id,))
+    results = cursor.fetchall()
+    
+    manga_details_list = []
     # query code goes here
+    if results:
+        for result in results:
+            # Constructs a dictionary with column names as keys
+            '''
+            published_start = result[2]
+            if published_start is not None:
+                published_start = result[2]
+            else:
+                published_start = None
+            published_end = result[3]
+            if published_end is not None:
+                published_end = result[3]
+            else:
+                published_end = None'''
+            manga_details = {
+                "title": result[0],
+                "author": result[1],
+                "published_start": result[2], #published_start,
+                "published_end": result[3], #published_end,
+                "themes": result[4],
+                "genres": result[5],
+                "synopsis": result[6]
+            }
+            manga_details_list.append(manga_details)
+    else:
+        manga_details_list = None
+
+        
 
     cursor.close()
-    return {
-        "nothing": "need to create func"
-    }
+    return manga_details_list
+    
